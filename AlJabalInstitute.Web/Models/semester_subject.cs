a47fa095d0f52d591d5ed42899298f926c4c8596
Reyad <reyad0669@gmail.com>
2026-01-09 01:22:08 +0200

    updatefull

---
+++ AlJabalInstitute.Web/Models/semester_subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace AlJabalInstitute.Web.Models
+{
+    [Table("semester_subjects")]
+    public class semester_subject : BaseModel
+    {
+        [PrimaryKey("id")]
+        public Guid Id { get; set; }
+
+        [Column("semester_id")]
+        public Guid SemesterId { get; set; }
+
+        [Column("subject_id")]
+        public Guid SubjectId { get; set; }
+    }
+}
